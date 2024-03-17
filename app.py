93e32fdf528121f02cb2884d1d1bfc437b887f1a
Danial Soleimany <102956315+DanialSoleimany@users.noreply.github.com>
2024-03-17 17:02:37 +0330

    app.py

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from PIL import Image
 from sklearn.preprocessing import LabelEncoder
+import requests
+from io import StringIO
 
 st.write("""
 # Iris Flower Web Application
@@ -15,24 +17,35 @@ image = Image.open(image_path)
 with st.expander("Iris Species Image"):
     st.image(image)
 
-data = pd.read_csv("Iris.csv")
-df = pd.DataFrame(data)
+# Kaggle dataset URL
+url = "https://www.kaggle.com/uciml/iris/download"
 
-with st.expander("Dataset"):
-    st.write(df)
+# Send a GET request to the URL
+response = requests.get(url)
+
+# Check if the request was successful
+if response.status_code == 200:
+    # Read the content of the response into a DataFrame
+    data = pd.read_csv(StringIO(response.text))
+    df = pd.DataFrame(data)
+    
+    with st.expander("Dataset"):
+        st.write(df)
+else:
+    st.error("Failed to fetch data from Kaggle.")
 
 def user_inputs():
     st.sidebar.subheader("Features of Iris Flower:")
-    sepal_length = st.sidebar.slider("Sepal Length (Cm)", df["SepalLengthCm"].min(), df["SepalLengthCm"].max())
-    sepal_width = st.sidebar.slider("Sepal Width (Cm)", df["SepalWidthCm"].min(), df["SepalWidthCm"].max())
-    petal_length = st.sidebar.slider("Petal Length (Cm)", df["PetalLengthCm"].min(), df["PetalLengthCm"].max())
-    petal_width = st.sidebar.slider("Petal Width (Cm)", df["PetalWidthCm"].min(), df["PetalWidthCm"].max())
+    sepal_length = st.sidebar.slider("Sepal Length (Cm)", df["sepal_length"].min(), df["sepal_length"].max())
+    sepal_width = st.sidebar.slider("Sepal Width (Cm)", df["sepal_width"].min(), df["sepal_width"].max())
+    petal_length = st.sidebar.slider("Petal Length (Cm)", df["petal_length"].min(), df["petal_length"].max())
+    petal_width = st.sidebar.slider("Petal Width (Cm)", df["petal_width"].min(), df["petal_width"].max())
 
     features = {
-        "SepalLengthCm": sepal_length,
-        "SepalWidthCm": sepal_width,
-        "PetalLengthCm": petal_length,
-        "PetalWidthCm": petal_width
+        "sepal_length": sepal_length,
+        "sepal_width": sepal_width,
+        "petal_length": petal_length,
+        "petal_width": petal_width
     }
 
     features_df = pd.DataFrame(features, index=[0])
@@ -43,8 +56,8 @@ features_df = user_inputs()
 with st.expander("Iris Features"):
     st.write(features_df)
 
-X = df.drop(["Id", "Species"], axis=1)
-y = df["Species"]
+X = df.drop(["species", "Id"], axis=1)
+y = df["species"]
 
 label_encoder = LabelEncoder()
 y_encoded = label_encoder.fit_transform(y)
